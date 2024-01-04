a5f0fc3523c190de82a727535df72fbe27742f42
Usamah Abdillah Hanif <usamahhanif3@gmail.com>
2024-01-04 09:43:11 +0700

    menambahkan routing routing lain untuk CRUD di web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/book', 'App\Http\Controllers\BookController@index');
-Route::get('/book/create', 'App\Http\Controllers\BookController@index');
-Route::get('/book', 'App\Http\Controllers\BookController@index');
-Route::get('/book', 'App\Http\Controllers\BookController@index');
-Route::get('/book', 'App\Http\Controllers\BookController@index');
+Route::get('/book/create', 'App\Http\Controllers\BookController@create');
+Route::get('/book/edit', 'App\Http\Controllers\BookController@edit');
+Route::get('/book/update', 'App\Http\Controllers\BookController@update');
+Route::get('/book/delete', 'App\Http\Controllers\BookController@delete');
